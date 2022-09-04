0ee81439ed6d0a5f146a5701265d20fcdc843c78
sellnat77 <sell_nat@yahoo.com>
2022-09-04 11:20:34 -0700

    Expose the cors url for the frontend

--- internal/handlers/cors.go
+++ internal/handlers/cors.go
@@ -3,13 +3,15 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/sellnat77/Decisionator-api/internal/util"
 )
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	corsAddr := util.GetEnvVar("CORS_URL")
+	(*w).Header().Set("Access-Control-Allow-Origin", corsAddr)
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
-
 }
 
 func Preflight(w http.ResponseWriter, req *http.Request) {
